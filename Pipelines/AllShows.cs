49a8393f58dfd542f8a0d60cbd80d793ca2e218e
Olly Levett <oliver.levett@softwire.com>
2020-09-01 09:33:06 +0100

    fix show content

--- Pipelines/AllShows.cs
+++ Pipelines/AllShows.cs
@@ -6,6 +6,7 @@ using Statiq.Markdown;
 using Statiq.Razor;
 using Statiq.Yaml;
 using System.Linq;
+using System.Threading.Tasks;
 
 namespace Sedos.Pipelines
 {
@@ -26,9 +27,10 @@ namespace Sedos.Pipelines
                 new SetMetadata("has-body-content", Config.FromDocument(d => d.ContentProvider.Length > 0)),
                 new SetMetadata("header-image", Config.FromDocument((doc, ctx) => HeaderImageExtensions.CopyAndResizeHeaderImage(doc,ctx))),
                 new SetMetadata("flyer", Config.FromDocument((doc, ctx) => HeaderImageExtensions.CopyAndResizeImageFromMeta(doc,ctx,"flyer" ,null, 360))),
-                new SetMetadata("sections", Config.FromDocument((doc, ctx) => doc.Get("sections", Enumerable.Empty<IDocument>())
-                    .OrderBy(d => d.Get("order", 1))
-                    .Select(d => MarkdownExtensions.ProcessMarkdownAsync(d, ctx)))),
+                new SetMetadata("sections", Config.FromDocument((doc, ctx) =>
+                    Task.WhenAll(doc.Get("sections", Enumerable.Empty<IDocument>())
+                        .OrderBy(d => d.Get("order", 1))
+                        .Select(d => MarkdownExtensions.ProcessMarkdownAsync(d, ctx))))), 
                 new RenderMarkdown()
                     .UseExtension<BootstrapExtension>()
                     .UseExtension<TargetLinkExtension>()
