1c53a911cde0c690b0ca9957456df9c2f34e5325
Olly Levett <oliver.levett@softwire.com>
2020-11-06 20:00:15 +0000

    feat: split out archive shows from main shows (#778)
    fixes #768

--- Pipelines/AllShows.cs
+++ Pipelines/AllShows.cs
@@ -16,15 +16,15 @@ namespace Sedos.Pipelines
 
             InputModules = new ModuleList
             {
-                new ReadFiles("shows/*.md")
+                new ReadFiles("shows/*.md", "archive-shows/*.md")
             };
 
             ProcessModules = new ModuleList
             {
                 new ExtractFrontMatter(new ParseYaml()),
                 new SetMetadata("has-body-content", Config.FromDocument(d => d.ContentProvider.Length > 0)),
-                new SetMetadata("header-image", Config.FromDocument((doc, ctx) => HeaderImageExtensions.CopyAndResizeHeaderImage(doc,ctx))),
-                new SetMetadata("flyer", Config.FromDocument((doc, ctx) => HeaderImageExtensions.CopyAndResizeImageFromMeta(doc,ctx,"flyer" ,null, 360))),
+                new SetMetadata("header-image", Config.FromDocument((doc, ctx) => HeaderImageExtensions.CopyAndResizeHeaderImage(doc, ctx))),
+                new SetMetadata("flyer", Config.FromDocument((doc, ctx) => HeaderImageExtensions.CopyAndResizeImageFromMeta(doc, ctx, "flyer", null, 360))),
                 new SetMetadata("sections", Config.FromDocument((doc, ctx) =>
                     Task.WhenAll(doc.Get("sections", Enumerable.Empty<IDocument>())
                         .OrderBy(d => d.Get("order", 1))
