ae58cb9e8d327fbc1a738548bf21f9ccc1a5dd0f
Adam Coppard <adam.coppard@gmail.com>
2021-01-30 14:43:18 +0000

    Fix home page ordering based on future dates

--- Pipelines/UpcomingEvents.cs
+++ Pipelines/UpcomingEvents.cs
@@ -15,8 +15,10 @@ namespace Sedos.Pipelines
             ProcessModules = new ModuleList
             {
                 new ReplaceDocuments(nameof(Events)),
-                new FilterDocuments(Config.FromDocument((doc, ctx) =>doc.Get("times", Enumerable.Empty<IDocument>()).Any(time => time.Get<DateTime>("time") > DateTime.Now))),
-                new OrderDocuments(Config.FromDocument((doc, ctx) => doc.Get("times", Enumerable.Empty<IDocument>()).Select(time => time.Get<DateTime>("time")).First()))
+                new FilterDocuments(Config.FromDocument((doc, ctx) => doc.Get("times", Enumerable.Empty<IDocument>()).Any(time => time.Get<DateTime>("time") > DateTime.Now))),
+                new OrderDocuments(Config.FromDocument((doc, ctx) => doc.Get("times", Enumerable.Empty<IDocument>())
+                        .Select(time => time.Get<DateTime>("time"))
+                        .First(time => time > DateTime.Now)))
             };
         }
     }
