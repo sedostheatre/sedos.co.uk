af06431c001d0dc3132392130c8e3c29d5dc5161
Adam Coppard <adam.coppard@gmail.com>
2021-05-08 19:20:17 +0100

    feat: add box office links

---
+++ BoxOfficeUri.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Sedos {
+    public static class BoxOfficeUri {
+        private static readonly Uri BASE = new Uri("https://sedos.ticketsolve.com/shows/");
+        public static Uri fromShowId(string? showId) {
+            return new Uri(BASE, showId);
+        }
+
+    }
+}
