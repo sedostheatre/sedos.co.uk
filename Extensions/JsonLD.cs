8179cce05bf005a88e36866e7aa54dc589027b8d
Olly Levett <oliver.levett@softwire.com>
2021-11-03 09:55:34 +0000

    feat: in schema.org skip location/date for shows which don't have them

--- Extensions/JsonLD.cs
+++ Extensions/JsonLD.cs
@@ -2,6 +2,7 @@ using Statiq.Common;
 using Schema.NET;
 using System;
 using System.Linq;
+using System.Collections.Generic;
 
 namespace Sedos.Extensions
 {
@@ -9,11 +10,19 @@ namespace Sedos.Extensions
     {
         public static string Show(IDocument doc)
         {
+            DateTimeOffset? startDate = doc.Get("showtimes", Enumerable.Empty<IDocument>()).Any()
+                ? doc.Get("showtimes", Enumerable.Empty<IDocument>()).Select(x => x.Get<DateTimeOffset>("time")).OrderBy(x => x).FirstOrDefault()
+                : null;
+
+            var location = doc.ContainsKey("venue")
+                ? new Place() { Address = doc.Get("venue", ""), Name = doc.Get("venue", "") }
+                : null;
+
             return new TheaterEvent()
             {
                 Name = doc.Get("title", ""),
-                StartDate = doc.Get("showtimes", Enumerable.Empty<IDocument>()).Select(x => x.Get<DateTimeOffset>("time")).OrderBy(x => x).FirstOrDefault(),
-                Location = new Place() { Address = doc.Get("venue", ""), Name = doc.Get("venue", "") },
+                StartDate = startDate,
+                Location = location,
                 SubEvent = new OneOrMany<IEvent>(doc.Get("showtimes", Enumerable.Empty<IDocument>()).Select(x => x.Get<DateTimeOffset>("time")).Select(x => new TheaterEvent()
                 {
                     Name = doc.Get("title", ""),
