7924c75eb63652eebca8949a0701dfc538eafa75
Adam Coppard <adamcoppard@users.noreply.github.com>
2021-11-17 13:32:49 +0000

    chore: add idiot custom redirects (#1417)

--- Pipelines/Redirects.cs
+++ Pipelines/Redirects.cs
@@ -1,6 +1,7 @@
 ﻿using Statiq.Common;
 using Statiq.Core;
 using System;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace Sedos.Pipelines
@@ -11,10 +12,17 @@ namespace Sedos.Pipelines
         {
             Dependencies.AddRange(nameof(AboutSections), nameof(AllShows), nameof(News), nameof(Index), nameof(RegularEvents), nameof(TopLevelPages), nameof(MarkdownPages));
 
+            var temporaryCustomRedirects = new Dictionary<string, string>() {
+                { "idiot/form", "https://docs.google.com/forms/d/e/1FAIpQLSfwwilNjpYp4LtkLaE2zBQGO4fUVG288TGuSqa6DpA1ifyQGQ/viewform" },
+                { "idiot/materials", "https://drive.google.com/drive/folders/161ZgKRV_ZACJqwSpKZeeHQW6110i-WEJ?usp=sharing" }
+            };
+
             ProcessModules = new ModuleList
             {
                 new ReplaceDocuments(nameof(AboutSections), nameof(AllShows), nameof(News), nameof(Index), nameof(RegularEvents), nameof(TopLevelPages), nameof(MarkdownPages)),
-                new GenerateRedirects().WithAdditionalOutput("_redirects", redirects => string.Join(Environment.NewLine, redirects.OrderBy(r => r.Key).Select(r => $"/{r.Key} {r.Value} 301!"))),
+                new GenerateRedirects().WithAdditionalOutput("_redirects", redirects =>
+                    string.Join(Environment.NewLine, redirects.OrderBy(r => r.Key).Select(r => $"/{r.Key} {r.Value} 301!")
+                        .Append(string.Join(Environment.NewLine, temporaryCustomRedirects.Select(r => $"/{r.Key} {r.Value} 302!"))))),
             };
 
             OutputModules = new ModuleList
