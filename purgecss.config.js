8f7a75dd8f579f553ccc651d818e00e3f6fe7514
Olly Levett <olly@sedos.co.uk>
2022-01-27 13:40:20 +0000

    feat: purge css on build (#1524)

---
+++ purgecss.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    content: ['output/**/*.html', 'output/**/*.htm'],
+    css: ['output/assets/css/main.css'],
+    output: 'output/assets/css/',
+    safelist: {
+        standard: [/carousel.*/]
+    }
+  }
