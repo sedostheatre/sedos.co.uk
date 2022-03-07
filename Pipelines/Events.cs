3b57de41b8192a19f58e6375974414f70bee2d0a
Olly Levett <olevett@gmail.com>
2022-03-07 20:34:16 +0000

    feat: add all events page
    fixes #1519

--- Pipelines/Events.cs
+++ Pipelines/Events.cs
@@ -1,4 +1,6 @@
-﻿using Sedos.Extensions;
+﻿using System;
+using System.Linq;
+using Sedos.Extensions;
 using Statiq.Common;
 using Statiq.Core;
 using Statiq.Html;
@@ -30,6 +32,7 @@ namespace Sedos.Pipelines
                 new SetMetadata("header-image", Config.FromDocument((doc, ctx) => HeaderImageExtensions.CopyAndResizeHeaderImage(doc,ctx))),
                 new SetMetadata("category", "events"),
                 new SetMetadata("background-override", "bg-purple"),
+                new SetMetadata("times", Config.FromDocument((doc, ctx) => doc.Get("times", Enumerable.Empty<IDocument>()).Select(t => t.Get<DateTime>("time")))),
                 new RenderRazor().WithViewStart("Layout/_EventViewStart.cshtml"),
             };
 
