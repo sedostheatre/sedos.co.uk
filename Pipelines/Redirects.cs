2e345e831cd4f2a527e80cf69b4213cc4ac73f78
Olly Levett <olly@sedos.co.uk>
2022-06-08 17:09:33 +0100

    feat: remove idiot custom redirects

--- Pipelines/Redirects.cs
+++ Pipelines/Redirects.cs
@@ -14,11 +14,7 @@ namespace Sedos.Pipelines
 
             var temporaryCustomRedirects = new Dictionary<string, string>() {
                 { "deiform", "https://forms.gle/MjYUCXrfMByr8D55A" },
-                { "newsletter", "https://mailchi.mp/sedos.co.uk/newsletter-sign-up" },
-                { "idiot/form", "https://docs.google.com/forms/d/e/1FAIpQLSfwwilNjpYp4LtkLaE2zBQGO4fUVG288TGuSqa6DpA1ifyQGQ/viewform" },
-                { "idiot/material", "https://drive.google.com/drive/folders/161ZgKRV_ZACJqwSpKZeeHQW6110i-WEJ?usp=sharing" },                
-                { "idiot/materials", "https://drive.google.com/drive/folders/161ZgKRV_ZACJqwSpKZeeHQW6110i-WEJ?usp=sharing" },
-                { "idiot/notice", "https://drive.google.com/file/d/1vROxdXRoRaENbBw9a-A_-19sl6d7IGah/view?usp=sharing" },
+                { "newsletter", "https://mailchi.mp/sedos.co.uk/newsletter-sign-up" },               
                 { "carrie/form", "https://docs.google.com/forms/d/e/1FAIpQLSdQciQq-Wmwivokfr1krlR4cMH8iR6O1rwkl6205R0gBlf6TQ/viewform?usp=sf_link" },
                 { "carrie/notice", "https://docs.google.com/document/d/1tBZGC5erP8yYSN-GJXR11TPhvIc746N0ZyU97deCyP4/edit?usp=sharing" },
                 { "carrie/materials", "https://drive.google.com/drive/folders/1IUe23s-DjXNRFFgZ_NvrSvpiC_QPq_83?usp=sharing" },
