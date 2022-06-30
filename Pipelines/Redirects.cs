567f8a4eabae5bb1906f3531554e4deee5108f59
Olly Levett <oliver.levett@softwire.com>
2022-06-30 18:05:05 +0100

    feat: remove unused exclamation mark from redirects file
    which likely makes it compatible with Cloudflare should we want to do that at any point...

--- Pipelines/Redirects.cs
+++ Pipelines/Redirects.cs
@@ -14,7 +14,7 @@ namespace Sedos.Pipelines
 
             var temporaryCustomRedirects = new Dictionary<string, string>() {
                 { "deiform", "https://forms.gle/MjYUCXrfMByr8D55A" },
-                { "newsletter", "https://mailchi.mp/sedos.co.uk/newsletter-sign-up" },               
+                { "newsletter", "https://mailchi.mp/sedos.co.uk/newsletter-sign-up" },
                 { "carrie/form", "https://docs.google.com/forms/d/e/1FAIpQLSdQciQq-Wmwivokfr1krlR4cMH8iR6O1rwkl6205R0gBlf6TQ/viewform?usp=sf_link" },
                 { "carrie/notice", "https://docs.google.com/document/d/1tBZGC5erP8yYSN-GJXR11TPhvIc746N0ZyU97deCyP4/edit?usp=sharing" },
                 { "carrie/materials", "https://drive.google.com/drive/folders/1IUe23s-DjXNRFFgZ_NvrSvpiC_QPq_83?usp=sharing" },
@@ -26,8 +26,8 @@ namespace Sedos.Pipelines
             {
                 new ReplaceDocuments(nameof(AboutSections), nameof(AllShows), nameof(News), nameof(Index), nameof(RegularEvents), nameof(TopLevelPages), nameof(MarkdownPages)),
                 new GenerateRedirects().WithAdditionalOutput("_redirects", redirects =>
-                    string.Join(Environment.NewLine, redirects.OrderBy(r => r.Key).Select(r => $"/{r.Key} {r.Value} 301!")
-                        .Append(string.Join(Environment.NewLine, temporaryCustomRedirects.Select(r => $"/{r.Key} {r.Value} 302!"))))),
+                    string.Join(Environment.NewLine, redirects.OrderBy(r => r.Key).Select(r => $"/{r.Key} {r.Value} 301")
+                        .Append(string.Join(Environment.NewLine, temporaryCustomRedirects.Select(r => $"/{r.Key} {r.Value} 302"))))),
             };
 
             OutputModules = new ModuleList
