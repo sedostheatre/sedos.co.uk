3de241be21bb42b4018c486a5b3ac0b7f92034f3
Adam Coppard <adamcoppard@users.noreply.github.com>
2023-07-20 11:33:52 +0100

    chore: add pitch library link (#2456)

--- Pipelines/Redirects.cs
+++ Pipelines/Redirects.cs
@@ -20,6 +20,7 @@ namespace Sedos.Pipelines
                 { "carrie/materials", "https://drive.google.com/drive/folders/1IUe23s-DjXNRFFgZ_NvrSvpiC_QPq_83?usp=sharing" },
                 { "pitch-guidance", "https://docs.google.com/document/d/1VmBuK-rIYb5z8f3G-pqA5W31GD68Z-HT57vnOg2KX2o/edit?usp=sharing" },
                 { "pitch-form", "https://docs.google.com/document/d/1cOV5QL3hYRbeBl7Zhm9HoreyuYjutE1XhILiNgYujpI/edit?usp=sharing" },
+                { "pitch-library", "https://drive.google.com/drive/folders/1GFwcyGoPsi7N8GQBBBlpg1lI7ex7O8_i?usp=drive_link" },
                 { "sunshine/material", "https://drive.google.com/drive/folders/1eMb1eu5-fZxbYa3fpgw10hoGoDrpsrD3?usp=sharing" },
                 { "sunshine/form", "https://forms.gle/59bymGMZ791kArsp8" },
                 { "sunshine/notice", "https://drive.google.com/file/d/1lfh7yP1i_8OmpmQeSJR_CyTglQ2V3J_G/view?usp=share_link" },
