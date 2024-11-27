2cf20fad1ca8044411689a0d87a9932a72112da1
Ben Hussey <ben@blip2.net>
2024-11-27 22:50:54 +0000

    hairspray auditions redirect

--- Pipelines/Redirects.cs
+++ Pipelines/Redirects.cs
@@ -45,6 +45,7 @@ namespace Sedos.Pipelines
                 { "september-2024-pitch-form", "https://docs.google.com/document/d/1tCxS9i3uRrWv7ksV6L_Yn_aUC5a0B40K9R9Da3c_AOY/edit" },
                 { "september-2024-pitch-guidance", "https://docs.google.com/document/d/1Iz6vsEuz4-Y1mClXD2l4PLRce4g761cn0Z6JfyK1Y5A/edit" },
                 { "charliebrown", "https://www.sedos.co.uk/shows/2024-you-re-a-good-man-charlie-brown" },
+                { "hairspray-auditions", "https://drive.google.com/drive/folders/1aHu2QhRjtSrqWYDMnF0F7l_35PmIk6Z-" },
             };
 
             ProcessModules = new ModuleList
